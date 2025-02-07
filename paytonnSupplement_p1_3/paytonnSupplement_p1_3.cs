d9763cba0b94f9637323a0bcb64d64f9e132cd2a
donp8on <paytonadonis@gmail.com>
2025-02-06 20:41:20 -0500

    chore: declared methods for creating and reading ascii and binary files

--- paytonnSupplement_p1_3/paytonnSupplement_p1_3.cs
+++ paytonnSupplement_p1_3/paytonnSupplement_p1_3.cs
@@ -1,7 +1,33 @@
-﻿namespace paytonnSupplement_p1_3
+﻿using System.Text;
+
+namespace paytonnSupplement_p1_3
 {
     public class paytonnSupplement_p1_3
     {
+        public static void CreateAsciiFile(string filePath, string content)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public static void CreateBinaryFile(string filePath, byte[] content)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public static string ReadAsciiFile(string filePath)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public static byte[] ReadBinaryFile(string filePath)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public static void CompareFileSizes()
+        {
+            throw new System.NotImplementedException();
+        }
 
     }
 }
